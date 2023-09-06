04fe66e3fff341190693b80b8b8db42dcaa5233f
İbrahim Aydın <ibayd@bilesim.net.tr>
2023-09-06 12:14:29 +0300

    ilkdüzenleme

--- WeatherForecastAPI/WeatherForecast.cs
+++ WeatherForecastAPI/WeatherForecast.cs
@@ -9,6 +9,8 @@ namespace WeatherForecastAPI
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
-        
+
+        public int deneme { get; set; }
+
     }
 }
